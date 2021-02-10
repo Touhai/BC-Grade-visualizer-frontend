6a49b1a40687c3a70940909dcf42bbca86e7727c
Bryan <bryanwkou@,gmail.com>
2021-02-10 09:35:03 -0800

    Added Header, added Formik

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,18 @@
+// Main header file in My App.
+
+import React from 'react'
+
+
+function Header(){
+    return(
+        
+        <ul className = "navigation-bar">
+           <li className = "navigation-bar-element">Home</li>
+           <li className = "navigation-bar-element">API</li>
+       </ul>
+        
+      
+    )
+}
+
+export default Header
